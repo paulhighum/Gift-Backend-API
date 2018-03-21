a6a796ee6c1f63c8454292033e8cbf7b6cea77d1
Paul Highum <phigh07@gmail.com>
2018-03-21 09:55:11 -0600

    updates migration, initial seed data

--- migrations/20180320135613_ideas.js
+++ migrations/20180320135613_ideas.js
@@ -2,9 +2,11 @@ exports.up = function(knex, Promise){
   return knex.schema.createTable("ideas", table => {
     table.increments("id").primary()
     table.text("idea")
+    table.text("occasion")
     table.integer("min-age")
     table.integer("max-age")
-    table.integer("relationship-length")
+    table.integer("min-relationship-length")
+    table.integer("max-relationship-length")
     table.text("gender")
   })
 }
--- seeds/01_ideas.js
+++ seeds/01_ideas.js
@@ -1,13 +1,38 @@
-
-exports.seed = function(knex, Promise) {
+exports.seed = function(knex, Promise){
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("ideas")
+    .del()
+    .then(function(){
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+      return knex("ideas").insert([
+        {
+          id: 1,
+          idea: "Flowers",
+          occasion: "Valentines Day",
+          "min-age": 10,
+          "max-age": 100,
+          "min-relationship-length": 0,
+          "max-relationship-length": 100,
+          gender: "Female"
+        },{
+          id: 2,
+          idea: "Chocolate",
+          occasion: "Valentines Day",
+          "min-age": 10,
+          "max-age": 100,
+          "min-relationship-length": 0,
+          "max-relationship-length": 50,
+          gender: "Female"
+        },{
+          id: 3,
+          idea: "Chocolate Covered Strawberries",
+          occasion: "Valentines Day",
+          "min-age": 12,
+          "max-age": 100,
+          "min-relationship-length": 0,
+          "max-relationship-length": 50,
+          gender: "Female"
+        },
+      ])
+    })
+}
